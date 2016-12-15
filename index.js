341f1e36a227144501721ea5f4cb24fb0cfda76f
Benjamin Huang <coolconfucius@yahoo.com>
2016-12-15 15:43:33 -0800

    quantity

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 console.log('indexjs');
-var mongoose  = require('mongoose'),
-    express   = require('express'),
+var express   = require('express'),
     bp        = require('body-parser'),
     path      = require('path'),
     root      = __dirname,
