43c4c8fdd6f05d6e09f3254d360cb55cf5687eec
Benjamin Huang <coolconfucius@yahoo.com>
2016-12-14 16:11:59 -0800

    '

--- client/assets/app.js
+++ client/assets/app.js
@@ -3,8 +3,11 @@ var app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
   .when('/', {
-    templateUrl: 'partials/list.html'
+    templateUrl: 'partials/dashboard.html'
   })
+
+
+
   .when('/new', {
     templateUrl: 'partials/new.html'
   })
